b94636179e13dd4d68f99485c5fc55a064f2d899
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2012-10-27 19:52:33 +0200

    Modified setup.py so that c files are properly compiled

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from numpy.distutils.core import setup
+import numpy
 
 
-setup(
-    name='bptomo',
-    packages=['bptomo'],
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("bptomo.tan_tan", ["bptomo/tan_tan.pyx"])]
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(None, parent_package, top_path)
+
+
+    config.add_subpackage('bptomo')
+
+    config.add_extension(
+        'bptomo.tan_tan',
+        sources=['bptomo/tan_tan.c'],
+        include_dirs=[numpy.get_include()],
     )
+
+
+    return config
+
+
+if __name__ == "__main__":
+
+    setup(configuration=configuration,
+          name='bptomo',
+          packages=['bptomo']
+          )
