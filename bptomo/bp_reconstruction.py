8f548c9efea3b13d8c4ac3edd34c5dbf4452217f
Emmanuelle Gouillart <emmanuelle.gouillart@nsup.org>
2012-10-27 10:57:43 +0200

    Minor changes (doc)...

--- bptomo/bp_reconstruction.py
+++ bptomo/bp_reconstruction.py
@@ -162,7 +162,7 @@ def BP_step_parallel(h_m_to_px, h_px_to_m, y, proj_operator, J=.1,
     """
     One iteration of BP (belief propagation), with messages updated
     after all new messages have been computed. A strong damping is needed
-    with this update method. This function is parallelized (used as many
+    with this update method. This function is parallelized (uses as many
     procs as possible !)
 
     Parameters
@@ -329,7 +329,7 @@ def BP_step_always_update(h_m_to_px, h_px_to_m, y, proj_operator,
         h_m_to_px_line, hext_new[measure_index] = \
                 solve_line(h_px_to_m[mu][inds], Js,
                     proj_value, use_micro=use_micro, hext=hext[measure_index])
-        damping = 0.5
+        damping = 0.3#5
         h_m_to_px_line = (1 - damping) * h_m_to_px_line \
                             + damping * h_tmp[mu][inds]
         h_m_to_px[mu][inds] = h_m_to_px_line[:]
@@ -392,6 +392,9 @@ def BP_step_update_direction(h_m_to_px, h_px_to_m, y, proj_operator,
         or chains with few non-blocked spins. If False, only canonical
         computations are performed.
 
+    hext : ndarray of same shape as y
+        Initial guess for the external fields.
+
     Returns
     -------
 
@@ -401,6 +404,20 @@ def BP_step_update_direction(h_m_to_px, h_px_to_m, y, proj_operator,
 
     h_sum is the sum over all directions of h_m_to_px, from which the marginal
     of a spin to be +1 or -1 can be computed.
+
+    Notes
+    -----
+    The steps performed here are as follows:
+
+    1. Loop over the projection angles
+    2. Loop over measures for a given projection angle
+    3. Compute effective couplings Js
+    4. Solve the line
+
+    Both types of fields (h_px_to_m and h_m_to_px) are in the image frame,
+    and indices corresponding to the different light-rays are extracted
+    from the projection operator.
+
     """
 
     ndir = len(h_m_to_px)
@@ -427,9 +444,116 @@ def BP_step_update_direction(h_m_to_px, h_px_to_m, y, proj_operator,
                 continue
             Js = _calc_Jeff(inds, l_x, J)
             h_dir_tmp[inds], hext_new[measure_index] = \
-                solve_line(h_px_to_m[direction][inds], Js, 
+                solve_line(h_px_to_m[direction][inds], Js,
                     proj_value, use_micro=use_micro, hext=hext[measure_index])
-        damping = 0.75
+        damping = 0.9
+        h_old = np.copy(h_m_to_px[direction])
+        h_dir_tmp = (1 - damping) * h_dir_tmp + damping * h_old
+        h_m_to_px[direction] = h_dir_tmp[:]
+        delta_h = h_dir_tmp - h_old
+        h_px_to_m = _calc_hatf_partial_dir(h_px_to_m, delta_h, direction)
+    h_sum = h_m_to_px.sum(axis=0)
+    h_sum[~mask] = 0
+    return h_m_to_px, h_px_to_m, h_sum, hext_new
+
+def BP_parallel_update_direction(h_m_to_px, h_px_to_m, y, proj_operator,
+                    J=.1, use_mask=True, use_micro=False, hext=None):
+    """
+    One iteration of BP (belief propagation), with messages updated
+    after all messages corresponding to one angle have been computed.
+
+    The directions are processed so that closeby directions are not processed
+    together. If there are ndir directions, they are processed in the order
+    i + j * ndir / 8 for j in range(0, 8), for i in range(0, ndir/8)
+
+    Parameters
+    ----------
+    h_m_to_px: ndarray of shape (ndir, l_x, l_x) where the original image
+        is (l_x, l_x)
+        local magnetizations (measure to field). The frame is the one of the
+        non-rotated image (ie, Ising chains are taken at an angle
+        corresponding to the projection)
+
+    h_px_to_m: ndarray of shape (ndir, l_x, l_x) where the original image
+        is (l_x, l_x)
+        local magnetic field, determined by the other measures. The frame
+        is the one of the non-rotated image (ie, Ising chains are taken at
+        an angle corresponding to the projection)
+
+    y: ndarray of shape ndir*L (1-D)
+        Array that contains all measures (sum of spins along rays)
+
+    proj_operator: sparse matrix, in lil format
+        The projection operator (with only 1s in non-zero entries)
+
+    J: float
+        Amplitude of the coupling between spins
+
+    use_mask: bool
+        If True, there are no spins outside the central circle
+
+    use_micro: bool
+        If True, a microcanonical computation is performed for short chains,
+        or chains with few non-blocked spins. If False, only canonical
+        computations are performed.
+
+    hext : ndarray of same shape as y
+        Initial guess for the external fields.
+
+    Returns
+    -------
+
+    h_m_to_px, h_px_to_m, h_sum
+
+    h_m_to_px and h_px_to_m are the new iterates
+
+    h_sum is the sum over all directions of h_m_to_px, from which the marginal
+    of a spin to be +1 or -1 can be computed.
+
+    Notes
+    -----
+    The steps performed here are as follows:
+
+    1. Loop over the projection angles
+    2. Loop over measures for a given projection angle
+    3. Compute effective couplings Js
+    4. Solve the line
+
+    Both types of fields (h_px_to_m and h_m_to_px) are in the image frame,
+    and indices corresponding to the different light-rays are extracted
+    from the projection operator.
+
+    """
+    if not joblib_import:
+        raise ImportError("""joblib could not be imported, use the
+        function BP_step instead""")
+    ndir = len(h_m_to_px)
+    if hext is None:
+        hext = np.zeros_like(y)
+    hext_new = np.zeros_like(y)
+    l_x = np.sqrt(h_m_to_px.shape[1])
+    h_tmp = np.copy(h_m_to_px)
+    if use_mask:
+        X, Y = np.ogrid[:l_x, :l_x]
+        mask = ((X - l_x/2)**2 + (Y - l_x/2)**2 <= (l_x/2)**2).ravel()
+    order = _order_dir(ndir)
+    for direction in order:
+        h_dir_tmp = np.zeros_like(h_px_to_m[direction])
+        for pixel_ind in range(int(l_x)):
+            measure_index = l_x * direction + pixel_ind
+            proj_value = y[measure_index]
+            inds = np.array(proj_operator.rows[measure_index])
+            if measure_index > len(y)/2:
+                inds = _reorder(inds, l_x)
+            mask_inds = mask[inds]
+            inds = inds[mask_inds]
+            if len(inds) == 0:
+                continue
+            Js = _calc_Jeff(inds, l_x, J)
+            h_dir_tmp[inds], hext_new[measure_index] = \
+                solve_line(h_px_to_m[direction][inds], Js,
+                    proj_value, use_micro=use_micro, hext=hext[measure_index])
+        damping = 0.9
         h_old = np.copy(h_m_to_px[direction])
         h_dir_tmp = (1 - damping) * h_dir_tmp + damping * h_old
         h_m_to_px[direction] = h_dir_tmp[:]
--- demo_blobs.py
+++ demo_blobs.py
@@ -1,13 +1,14 @@
 import numpy as np
 from scipy import sparse
-from bptomo.bp_reconstruction import BP_step, _initialize_field, _calc_hatf
+from bptomo.bp_reconstruction import BP_step, _initialize_field, _calc_hatf, \
+                            BP_step_update_direction
 from bptomo.build_projection_operator import build_projection_operator
 from bptomo.util import generate_synthetic_data
 import matplotlib.pyplot as plt
 
 # Generate synthetic data
-L = 128
-im = generate_synthetic_data(L)
+L = 256
+im = generate_synthetic_data(L, n_pts=100)
 im -= 0.5
 im *= 2
 
@@ -17,11 +18,12 @@ im[~mask] = 0
 
 
 # Build projection data
-n_dir = 24
+n_dir = L / 5
 op = build_projection_operator(L, n_dir)
 y = (op * im.ravel()[:, np.newaxis]).ravel()
 # Add some noise
-y += 1.*np.random.randn(*y.shape)
+np.random.seed(0)
+y += 4*np.random.randn(*y.shape)
 op = sparse.lil_matrix(op)
 
 # Prepare fields
@@ -29,19 +31,29 @@ sums = [] # total magnetization
 
 h_m_to_px = _initialize_field(y, op) # measure to pixel
 h_px_to_m, first_sum = _calc_hatf(h_m_to_px) # pixel to measure
-hext = np.zeros_like(y) # external field
+h_ext = np.zeros_like(y) # external field
 
+px_to_m, m_to_px = [], []
 
+err_measure = []
 
-for i in range(12):
+for i in range(20):
     print "iter %d" %i
-    h_m_to_px, h_px_to_m, h_sum, h_ext = BP_step(h_m_to_px,
-                                    h_px_to_m, y, op, hext=hext)
+    h_m_to_px, h_px_to_m, h_sum, h_ext = BP_step_update_direction(h_m_to_px,
+                                    h_px_to_m, y, op, J=0.1, hext=h_ext)
     sums.append(h_sum)
+    m_to_px.append(h_m_to_px)
+    px_to_m.append(h_px_to_m)
+    segmentation = np.sign(h_sum.reshape(L, L))
+    segmentation[~mask] = 0
+    #y_segmentation = (op * segmentation.ravel()[:, np.newaxis]).ravel()
+    #err_measure.append(((y - y_segmentation)**2).sum())
 
 err = [np.abs((sumi>0) - (im>0).ravel()).sum() for sumi in sums]
 print("number of errors vs. iteration: ")
 print(err)
+
+flip = [np.abs(np.sign(sums[i]) - np.sign(sums[i+1])).sum() for i in range(21)]
 
 plt.figure(figsize=(12, 4))
 plt.subplot(131)
@@ -54,6 +66,7 @@ plt.axis('off')
 plt.title('local magnetization')
 plt.subplot(133)
 plt.semilogy(err, 'o', ms=8)
+#plt.semilogy(err_measure, 'o', ms=8)
 plt.xlabel('$n$', fontsize=18)
 plt.title('# of errors')
 
